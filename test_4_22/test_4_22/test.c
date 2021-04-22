298b1c20c20110ba93888b34e0de18002f5e22b0
PTY963 <2924853846@qq.com>
2021-04-22 22:14:53 +0800

    学习

---
+++ test_4_22/test_4_22/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	printf("Practice make prefect!");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("v   v\n");
+//	printf(" v v\n");
+//	printf("  v\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("v   v\n v v\n  v\n");
+//	return 0;
+//}
+
+//#include<string.h>
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\040\test.c"));//13 \t-制表符 \ddd d八进制
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("0%o 0x%x", 1234, 1234);//02322 0x4d2 %o表示八进制 不是%0
+//	return 0;
+//}
+
+//int main()
+//{
+//    int a = 10;
+//	printf("%p\n", &a);//把0也打印出来 大写
+//	printf("%x\n", &a);//把前面的0省去，认为0没有意义 小写
+//	return 0;
+//}
