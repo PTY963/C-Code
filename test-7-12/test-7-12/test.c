3d1cee0eca1d788d0fc5b467bcf8e60421b629a2
PTY963 <2924853846@qq.com>
2021-07-12 13:04:40 +0800

    暑假作业5

---
+++ test-7-12/test-7-12/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main() 
+//{
+//	char arr[2][4];
+//	strcpy((char*)arr, "you");
+//	strcpy(arr[1], "me");
+//	arr[0][3] = '&';
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//-27 补码形式
+//10000000 00000000 00000000 0001 1011
+//11111111 11111111 11111111 1110 0100
+//11111111 11111111 11111111 1110 0101 补码
+
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	p = NULL;
+//	printf("%d\n", *p);
+//	return 0;
+//}
+
+//a + b * a + b
+//* 的优先级高于 +
+//1 + 2 * 1 + 2 
+
